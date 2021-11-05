33a37660507da71296ff3400698016fbed8d5727
Julio Angulo <solojoas@gmail.com>
2021-11-05 19:16:39 -0400

    Added problem 45

--- solutions/euler_lib/sequences.py
+++ solutions/euler_lib/sequences.py
@@ -1,5 +1,14 @@
 import math
 
+def triangle(n: int) -> int:
+    return int(n*(n+1)/2)
+
+def pentagonal(n: int) -> int:
+    return int(n*(3*n-1)/2)
+
+def hexagonal(n: int) -> int:
+    return int(n*(2*n - 1))
+
 def spiral_seq(n):
     s = 1
     for k in range(n):
