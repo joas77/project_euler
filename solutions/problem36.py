05144a2590c44d61f58b968b30ecf92607723cc7
Julio Angulo <solojoas@gmail.com>
2026-05-17 23:54:55 +0200

    solved problem 36

--- solutions/problem36.py
+++ solutions/problem36.py
@@ -5,17 +5,16 @@ The decimal number, 585 = 0b1001001001 (binary), is palindromic in both bases.
 Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
 (Please note that the palindromic number, in either base, may not include leading zeros.)
 """
+from euler_lib import numbers
 
 if __name__ == "__main__":
-    double_base_pals = [1,3,5,7,9]
+    double_base_pals = []
 
-    for i in range(1, 100):
-       pal_str = str(i) + str(i)[::-1]
-       pal = int(pal_str)
-       binary = bin(pal)[2::]
-
-       if binary == binary[::-1]:
-           double_base_pals.append(pal)
+    for i in range(1, 1_000_000):
+        if numbers.is_palindrome(i):
+            binstr = bin(i)[2:]
+            if binstr == binstr[::-1]:
+                double_base_pals.append(i)
 
     print(f"double base palindromes: {double_base_pals}")
 
